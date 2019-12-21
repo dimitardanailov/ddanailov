f368bb089bd4afbcc19eebef3de943bf1555568d
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-12-21 22:53:22 +0200

    Information about Open source projects.

---
+++ website/components/OpenSourceProjects.js
@@ -0,0 +1,59 @@
+function OpenSourceProjects() {
+  return (
+    <div>
+      <h3>Open source projects 🦉</h3>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/dimitardanailov/gmail-helper"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Native web componets consultant"
+          >
+            Gmail Helper
+          </a>
+          : Gmail helper creates gmail labels and features. Technologies: Custom
+          Elements v1, Redux, Webpack, Lerna, Mocha, Chai, Karma
+        </li>
+        <li>
+          <a
+            href="https://github.com/dimitardanailov/tantalus"
+            title="Nodejs consultant"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tantalus
+          </a>
+          is a Nodejs Microservices application. Project uploads big streams
+          from custom cloud provider to AWS. Tantalus uses: NodeServer
+          Generator, routing-controllers, tus.io, Typescript.
+        </li>
+        <li>
+          <a
+            href="https://github.com/dimitardanailov/push-notifications"
+            title="Push notification demo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Push notification playground
+          </a>
+          : Google Chrome and Firefox push notifications playground, Docker
+          Safari APN creator
+        </li>
+        <li>
+          <a
+            href="https://github.com/dimitardanailov/react-components"
+            title="React playground"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React components
+          </a>
+          : React catalog with different useful components.
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default OpenSourceProjects;
