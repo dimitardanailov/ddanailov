f418236ac77c6f354412c05a18aa742673be221a
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-12-21 22:52:40 +0200

    Application has a header.

---
+++ website/components/MainHeader.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+function MainHeader() {
+  return (
+    <div>
+      <Head>
+        <title>
+          Danailov Consulting: Javascript Consultant and Remote architect
+        </title>
+        <meta
+          name="description"
+          content="Javascript Consultant and Remote architect"
+        />
+        <meta name="author" content="Dimitar Danailov" />
+        <meta charSet="utf-8" />
+      </Head>
+    </div>
+  );
+}
+
+export default MainHeader;
