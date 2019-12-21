b2e7524a9517b33b5bf4dae71b612f3c79dc49eb
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-12-21 22:50:25 +0200

    Component has contact details and version.

---
+++ website/components/MainFooter.js
@@ -0,0 +1,18 @@
+function MainFooter() {
+  return (
+    <footer>
+      <p>
+        Contacts:
+        <a
+          href="mailto:dimityr.danailov@gmail.com"
+          title="Javascript Consultant"
+        >
+          dimityr.danailov@gmail.com
+        </a>
+      </p>
+      <p>Website: v1.2.0</p>
+    </footer>
+  );
+}
+
+export default MainFooter;
