851ccc168a9d90839d0a28379208fc2eb4b5f385
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-12-21 22:53:51 +0200

    Details about skills.

---
+++ website/components/Skills.js
@@ -0,0 +1,30 @@
+function Skills() {
+  return (
+    <div>
+      <h3>Skills</h3>
+      <ul>
+        <li>
+          🔥 React and Monorepo, Redux, Custom Elements v1, D3js, Safari Push
+          notifications, Polymer, Draftjs
+        </li>
+        <li>🛡 Webpack, Gulp, Lerna, Bash Script, Docker and Vagrant</li>
+        <li>🗳 Nodejs, PHP, Golang</li>
+        <li>🚓 Web technologies (HTML5, CSS3, ES7+, a11y)</li>
+        <li>
+          ⛅️ Azure, AWS, AWS Lambda functions and Serverless, Google Cloud and
+          Kubernates, Experience with monitoring tools: Kusto Query language
+        </li>
+        <li>💥 Typescript, Flow, Prettier</li>
+        <li>🍇 Feature Flags, Microservices, Fitness functions</li>
+        <li>
+          🍸 MongoDB (Mongoose), Firebase, SQlite, MySQL and MSSQL,
+          Hadoop(jinior)
+        </li>
+        <li>🔋 Travic CI, Circle CI, Jenkins, Groovy</li>
+        <li>🎧 Android, Swift and Objective - C (jinior)</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Skills;
