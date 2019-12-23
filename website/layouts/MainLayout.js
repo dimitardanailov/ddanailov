ec4558ae293b61a0358083ee7d1b2277654d8d29
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-12-23 23:26:51 +0200

    Layout could be dynamic.

--- website/layouts/MainLayout.js
+++ website/layouts/MainLayout.js
@@ -1,20 +1,12 @@
 import MainHeader from '../components/MainHeader';
-import PersonalStory from '../components/PersonalStory';
-import OpenSourceProjects from '../components/OpenSourceProjects';
-import Skills from '../components/Skills';
 import MainFooter from '../components/MainFooter';
 
-function MainLayout() {
-  return (
-    <div>
-      <MainHeader />
-      <h1>Dear Guest,</h1>
-      <PersonalStory />
-      <OpenSourceProjects />
-      <Skills />
-      <MainFooter />
-    </div>
-  );
-}
+const MainLayout = Page => () => (
+  <div>
+    <MainHeader />
+    <Page />
+    <MainFooter />
+  </div>
+);
 
 export default MainLayout;
