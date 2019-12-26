ea11d67902d86b8a2d078240ab622759e77b110b
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-12-26 23:48:54 +0200

    PersonalStory has a new format.

--- website/components/PersonalStory.js
+++ website/components/PersonalStory.js
@@ -1,32 +1,32 @@
 function PersonalStory() {
   return (
     <div>
-      <p>
+      <div>
         My personal journey begun before ten years ago. For these period I
         "visited" a signification amount of kingdoms 👑. My roles were "warrior"
         (backend developer), "engineer" (front - end developer), "magician"
         (team leader and software architect), "the assassin" (freelancer) and
         even the "main officer" (CTO).
-      </p>
-      <p>
+      </div>
+      <div>
         When I worked like a warrior 💪 I wrote a significant amount of scripts
         which automated some boring repetitive tasks for my stakeholders and the
         end - users.
-      </p>
-      <p>
+      </div>
+      <div>
         To be an engineer 🛠 is an awesome job because your main focus is
         creating a beautiful and helpful instruments which reduce the main pain
         of each system: Adding or filtering data.
-      </p>
-      <p>
+      </div>
+      <div>
         The magician role 🧙‍♀️ was a tough and motivational task. My leadership
         skills cut the deadlines, improve code quality and the final result was:
         strong and long partnership.
-      </p>
-      <p>
+      </div>
+      <div>
         Like an assassin 🔪 I learned how to be a solo player. I improved my
         skills to complete: fast and efficient stakeholders requirements.
-      </p>
+      </div>
     </div>
   );
 }
