787cb3342d437146144eede29ceadd8666c4e08a
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-12-26 23:49:52 +0200

    Skills component has a new version about my cloud and mobile skills.

--- website/components/Skills.js
+++ website/components/Skills.js
@@ -5,23 +5,22 @@ function Skills() {
       <ul>
         <li>
           🔥 React and Monorepo, Redux, Custom Elements v1, D3js, Safari Push
-          notifications, Polymer, Draftjs
+          notifications, Polymer, Draftjs;
         </li>
-        <li>🛡 Webpack, Gulp, Lerna, Bash Script, Docker and Vagrant</li>
-        <li>🗳 Nodejs, PHP, Golang</li>
-        <li>🚓 Web technologies (HTML5, CSS3, ES7+, a11y)</li>
+        <li>🛡 Webpack, Gulp, Lerna, Bash Script, Docker and Vagrant;</li>
+        <li>🗳 Nodejs, PHP, Golang;</li>
+        <li>🚓 Web technologies (HTML5, CSS3, ES7+, a11y);</li>
         <li>
           ⛅️ Azure, AWS, AWS Lambda functions and Serverless, Google Cloud and
-          Kubernates, Experience with monitoring tools: Kusto Query language
+          Kubernates, Experience with monitoring tools: Kusto Query language;
         </li>
-        <li>💥 Typescript, Flow, Prettier</li>
-        <li>🍇 Feature Flags, Microservices, Fitness functions</li>
+        <li>💥 Typescript, Flow, Prettier;</li>
+        <li>🍇 Feature Flags, Microservices, Fitness functions;</li>
         <li>
-          🍸 MongoDB (Mongoose), Firebase, SQlite, MySQL and MSSQL,
-          Hadoop(jinior)
+          🍸 MongoDB (Mongoose), Firebase, SQlite, MySQL and MSSQL, Hadoop;
         </li>
-        <li>🔋 Travic CI, Circle CI, Jenkins, Groovy</li>
-        <li>🎧 Android, Swift and Objective - C (jinior)</li>
+        <li>🔋 Travic CI, Circle CI, Jenkins, Groovy;</li>
+        <li>🎧 Android, Swift and Objective - C;</li>
       </ul>
     </div>
   );
