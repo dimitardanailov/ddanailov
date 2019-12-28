c54c5278b6042f1d391fbbe7a39c0b7a97bc6a70
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-12-28 21:28:16 +0200

    nextjs is able to load .gif

---
+++ website/next.config.js
@@ -0,0 +1,17 @@
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+        },
+      },
+    });
+
+    return config;
+  },
+});
