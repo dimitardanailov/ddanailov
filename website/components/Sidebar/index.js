721099d60643c46b97fb89a576578647429c094b
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-12-29 23:06:22 +0200

    Sidebar has a simple message and background.

--- website/components/Sidebar/index.js
+++ website/components/Sidebar/index.js
@@ -1,5 +1,32 @@
+import styled from 'styled-components';
+import img from './black.gif';
+
+const Wrapper = styled.div`
+  position: relative;
+
+  display: block;
+  background-color: #1d1d1d;
+  background-image: url(${img});
+`;
+
+const Text = styled.h4`
+  position: relative;
+
+  display: block;
+  color: #f9f9f9;
+  text-align: center;
+
+  text-shadow: 0 0 4px #000;
+
+  font-size: 2.75em;
+`;
+
 function Sidebar() {
-  return <div>Hello world</div>;
+  return (
+    <Wrapper>
+      <Text>dimitar danailov (a.k.a mitco)</Text>
+    </Wrapper>
+  );
 }
 
 export default Sidebar;
