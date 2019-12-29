e709bc8a9e1de2839243ff3a9a72ba52c48565ef
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-12-29 23:05:33 +0200

    Webpage uses Sidebar.

--- website/pages/index.js
+++ website/pages/index.js
@@ -3,9 +3,11 @@ import MainLayout from '../layouts/MainLayout';
 import PersonalStory from '../components/PersonalStory';
 import OpenSourceProjects from '../components/OpenSourceProjects';
 import Skills from '../components/Skills';
+import Sidebar from '../components/Sidebar';
 
 const Page = () => (
   <div>
+    <Sidebar />
     <h1>Dear Guest,</h1>
     <PersonalStory />
     <OpenSourceProjects />
