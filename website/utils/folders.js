bb9798a05ffd187450f4fcb7b4a5eb25f071bd84
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-01-01 23:14:41 +0200

    utils has details about the folder structure.

---
+++ website/utils/folders.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const BASE_DIR = __dirname.replace('utils', '');
+
+module.exports.BASE_DIR = BASE_DIR;
+module.exports.PUBLIC_DIR = path.resolve(BASE_DIR, 'public');
+module.exports.STATIC_DIR = path.resolve(BASE_DIR, 'static');
