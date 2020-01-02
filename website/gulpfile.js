5e15855da43ee56a0d3d764f628ec23796be3ba3
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-01-02 23:55:41 +0200

    util uses the build number.

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,9 +1,15 @@
 const { src, dest, series } = require('gulp');
 
 const { PUBLIC_DIR, STATIC_DIR } = require('./utils/folders');
+const getBuildNumber = require('./utils/nextjs/getBuildNumber');
+
+async function helloWorld() {
+  const buildNumber = await getBuildNumber();
+  console.log('build number:', buildNumber);
+}
 
 function copyPage404() {
   return src(`${STATIC_DIR}/404.html`).pipe(dest(PUBLIC_DIR));
 }
 
-exports.default = series(copyPage404);
+exports.default = series(helloWorld, copyPage404);
