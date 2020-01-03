c666d3b645151cdddf1c78b2a30dc3893fde3bd6
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-01-03 20:06:25 +0200

    Application contains information about .next folder.

--- website/utils/folders.js
+++ website/utils/folders.js
@@ -5,3 +5,4 @@ const BASE_DIR = __dirname.replace('utils', '');
 module.exports.BASE_DIR = BASE_DIR;
 module.exports.PUBLIC_DIR = path.resolve(BASE_DIR, 'public');
 module.exports.STATIC_DIR = path.resolve(BASE_DIR, 'static');
+module.exports.NEXT_BUILD_DIR = path.resolve(BASE_DIR, '.next');
