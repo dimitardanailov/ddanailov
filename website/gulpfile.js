0d3d796cd2f618f234044433ebd5d058fe758375
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-01-03 20:13:10 +0200

    copyNextResources is able to get content and copy in public folder.

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,15 +1,17 @@
 const { src, dest, series } = require('gulp');
 
-const { PUBLIC_DIR, STATIC_DIR } = require('./utils/folders');
+const { PUBLIC_DIR, STATIC_DIR, NEXT_BUILD_DIR } = require('./utils/folders');
 const getBuildNumber = require('./utils/nextjs/getBuildNumber');
 
-async function helloWorld() {
+async function copyNextResources() {
   const buildNumber = await getBuildNumber();
-  console.log('build number:', buildNumber);
+  const path = `${NEXT_BUILD_DIR}/server/static/${buildNumber}/pages/*.{js,html}`;
+
+  return src(path).pipe(dest(PUBLIC_DIR));
 }
 
 function copyPage404() {
   return src(`${STATIC_DIR}/404.html`).pipe(dest(PUBLIC_DIR));
 }
 
-exports.default = series(helloWorld, copyPage404);
+exports.default = series(copyNextResources, copyPage404);
