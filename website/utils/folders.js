29fa4b937562f018ccf5204db46cf39d65aae269
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-01-04 23:43:15 +0200

    utils folder has details about each folder.

--- website/utils/folders.js
+++ website/utils/folders.js
@@ -3,6 +3,19 @@ const path = require('path');
 const BASE_DIR = __dirname.replace('utils', '');
 
 module.exports.BASE_DIR = BASE_DIR;
-module.exports.PUBLIC_DIR = path.resolve(BASE_DIR, 'public');
+
+/**
+ * STATIC_DIR contains all static website files
+ */
 module.exports.STATIC_DIR = path.resolve(BASE_DIR, 'static');
+
+/**
+ * NEXT_BUILD_DIR is the core of the build process.
+ * The folder is used by nextjs
+ */
 module.exports.NEXT_BUILD_DIR = path.resolve(BASE_DIR, '.next');
+
+/**
+ * PUBLIC_DIR stores files which should be deployed on firebase.
+ */
+module.exports.PUBLIC_DIR = path.resolve(BASE_DIR, 'public');
