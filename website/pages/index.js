b262541e70c627137b421d4560b4607e34657460
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-01-08 22:06:24 +0200

    Application support desktop template.

--- website/pages/index.js
+++ website/pages/index.js
@@ -7,21 +7,33 @@ import OpenSourceProjects from '../components/OpenSourceProjects';
 import Skills from '../components/Skills';
 import Sidebar from '../components/Sidebar';
 
+const PageTemplate = styled.section`
+  position: relative;
+
+  display: flex;
+  flex-direction: row-reverse;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
 const PageHolder = styled.section`
   position: relative;
   margin: 1rem 1.2rem;
 `;
 
 const Page = () => (
-  <div>
+  <PageTemplate>
     <Sidebar />
+
     <PageHolder>
       <h1>Dear Guest,</h1>
       <PersonalStory />
       <OpenSourceProjects />
       <Skills />
     </PageHolder>
-  </div>
+  </PageTemplate>
 );
 
 export default MainLayout(Page);
