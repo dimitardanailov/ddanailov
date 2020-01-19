d22dcecc7272529ff065bc8d2071ac39152dc04d
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-01-19 12:30:01 +0200

    Footer has styles and every is align center.

--- website/components/MainFooter.js
+++ website/components/MainFooter.js
@@ -1,17 +1,22 @@
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  position: relative;
+
+  text-align: center;
+`;
+
 function MainFooter() {
   return (
-    <footer>
-      <p>
-        Contacts:
-        <a
+    <Footer>
+      <div>
+        Contacts: <a
           href="mailto:dimityr.danailov@gmail.com"
           title="Javascript Consultant"
-        >
-          dimityr.danailov@gmail.com
-        </a>
-      </p>
-      <p>Website: v1.5.0</p>
-    </footer>
+        >dimityr.danailov@gmail.com</a>
+      </div>
+      <div>Website: v1.5.0</div>
+    </Footer>
   );
 }
 
