0e5b8c6d7ca7b16f01ceefd88bc09f36c074d188
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-01-30 13:49:09 +0200

    Contact has title, placeholders, button has basic style. Input boxes are looking good with the new design.

--- website/components/FormComponents/ContactForm.js
+++ website/components/FormComponents/ContactForm.js
@@ -7,6 +7,16 @@ const Wrapper = styled.section`
   flex-direction: column;
 `;
 
+const ContactFormTitle = styled.span`
+  position: relative;
+
+  display: block;
+  margin: .2rem 0;
+  
+  font-size: 1.4rem;
+  text-align: center;
+`;
+
 const FieldSet = styled.fieldset`
   position: relative;
 
@@ -18,7 +28,12 @@ const FieldSet = styled.fieldset`
 const InputBox = styled.input`
   position: relative;
 
+  display: block;
   width: 100%;
+  padding: 5px 0;
+  border-radius: .25rem;
+  text-indent: .2em;
+  font-size: .9rem;
 `;
 
 const TextArea = styled.textarea`
@@ -29,6 +44,28 @@ const TextArea = styled.textarea`
   padding: 20px 0;
 
   border-radius: 1rem;
+  font-size: .9rem;
+  text-indent: .2em;
+`;
+
+const FieldSetButton = styled.fieldset`
+  position: relative;
+
+  display: flex;
+  border: none;
+`;
+
+const Button = styled.button`
+  position: relative;
+
+  display: block;
+  width: 40%;
+  margin: 0 auto;  
+  padding: .45rem;
+
+  font-size: 1rem;
+  background-color: #ccc;
+  border-radius: .2rem; 
 `;
 
 function ContactForm() {
@@ -38,6 +75,8 @@ function ContactForm() {
 
   return (
     <Wrapper>
+      <ContactFormTitle>I want to meet with you</ContactFormTitle>
+
       <FieldSet>
         <InputBox type="text" placeholder="What's your name" />
       </FieldSet>
@@ -47,14 +86,16 @@ function ContactForm() {
       </FieldSet>
 
       <FieldSet>
-        <InputBox type="text" placeholder="Subject" />
+        <InputBox type="text" placeholder="Message title or subject" />
       </FieldSet>
 
       <FieldSet>
-        <TextArea />
+        <TextArea placeholder="How I can help you ?" />
       </FieldSet>
 
-      <button onClick={onclickLister}>Contact me!!!</button>
+      <FieldSetButton>
+        <Button onClick={onclickLister}>Contact me!!!</Button>  
+      </FieldSetButton>
     </Wrapper>
   );
 }
