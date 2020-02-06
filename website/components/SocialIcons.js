1f8a5e357bd1712b133231ea28f7e988e5ea7c38
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-02-06 21:20:10 +0100

    Info about my social activities.

---
+++ website/components/SocialIcons.js
@@ -0,0 +1,59 @@
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+import styled from 'styled-components';
+
+const List = styled.ul`
+  position: relative;
+
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  display: flex;
+  flex-direction: row;
+`;
+
+const ListItem = styled.li`
+  position: relative;
+  margin-right: 1rem;
+`;
+
+function SocialIcons() {
+  return (
+    <div>
+      <List>
+        <ListItem>
+          <a
+            href="https://twitter.com/d_danailov"
+            title="Dimitar Danailov: Twitter account"
+            target="_blank"
+          >
+            <TwitterIcon fontSize="large" />
+          </a>
+        </ListItem>
+        <ListItem>
+          <a
+            href="https://www.linkedin.com/in/ddanailov/"
+            title="Dimitar Danailov: Twitter account"
+            target="_blank"
+          >
+            <LinkedInIcon fontSize="large" />
+          </a>
+        </ListItem>
+        <ListItem>
+          <a
+            href="https://github.com/dimitardanailov"
+            title="Dimitar Danailov: Github account"
+            target="_blank"
+          >
+            <GitHubIcon fontSize="large" />
+          </a>
+        </ListItem>
+      </List>
+    </div>
+  );
+}
+
+export default SocialIcons;
