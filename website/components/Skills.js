ae74f6564389e80fc9261d156933359471a7ff87
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-03-07 20:12:41 +0200

    Several skills have keyword highlight.

--- website/components/Skills.js
+++ website/components/Skills.js
@@ -1,20 +1,28 @@
+import Keyword from './Keyword'
+
 function Skills() {
   return (
     <div>
       <h3>Skills</h3>
       <ul>
         <li>
-          🔥 React and Monorepo, Redux, State Machines, Custom Elements v1,
-          D3js, Safari Push notifications, Polymer, Draftjs;
+          🔥 <Keyword word="React" /> and Monorepo, Redux, State Machines,
+          Custom Elements v1, D3js, Safari Push notifications, Polymer, Draftjs;
         </li>
         <li>🛡 Webpack, Gulp, Lerna, Bash Script, Docker and Vagrant;</li>
-        <li>🗳 Nodejs, PHP, Golang;</li>
+        <li>
+          🗳 Nodejs, PHP, <Keyword word="Golang" />;
+        </li>
         <li>🚓 Web technologies (HTML5, CSS3, ES7+, a11y);</li>
         <li>
-          ⛅️ Azure, AWS, AWS Lambda functions and Serverless, Google Cloud and
-          Kubernetes, Experience with monitoring tools: Kusto Query language;
+          ⛅️ <Keyword word="Azure" />, <Keyword word="AWS" />, AWS Lambda
+          functions and Serverless, <Keyword word="Google Cloud" /> and{' '}
+          <Keyword word="Kubernetes" />, Experience with monitoring tools: Kusto
+          Query language;
         </li>
-        <li>💥 Typescript, Flow, Prettier;</li>
+        <li>
+          💥 <Keyword word="Typescript" />, Flow, Prettier;
+        </li>
         <li>🍇 Feature Flags, Microservices, Fitness functions;</li>
         <li>
           🍸 MongoDB (Mongoose), Firebase, SQlite, MySQL and MSSQL, Hadoop;
