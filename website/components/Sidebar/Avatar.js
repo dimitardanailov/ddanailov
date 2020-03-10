45998fb208bd7d0bfd6c98f614afe505a7960315
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-03-10 21:48:06 +0200

    Element has onload feature to test browser device.

--- website/components/Sidebar/Avatar.js
+++ website/components/Sidebar/Avatar.js
@@ -1,3 +1,5 @@
+import * as React from 'react'
+
 import styled from 'styled-components'
 
 import avatar from './ddanailov-avatar.jpg'
@@ -13,12 +15,59 @@ const Wrapper = styled.img`
   border-radius: 12.5%;
 `
 
-function Avatar() {
+/**
+ * Resources:
+ * - https://davidwalsh.name/detect-iphone
+ */
+
+function isIphone() {
+  return window.navigator.userAgent.match(/iPhone/i)
+}
+
+function isPad() {
+  return window.navigator.userAgent.match(/iPad/i)
+}
+
+function isAndroid() {
+  return window.navigator.userAgent.match(/Android/i)
+}
+
+function detectDevice() {
+  if (isPad()) {
+    return 'tablet'
+  }
+
+  if (isIphone() || isAndroid()) {
+    return 'mobile'
+  }
+
+  return 'desktop'
+}
+
+function Image() {
   const title = 'Javascript consultant'
 
   return (
     <Wrapper src={avatar} title={title} alt={title} width="240" height="180" />
   )
+}
+
+function Avatar() {
+  // const device = detectDevice()
+
+  React.useEffect(() => {
+    // window is accessible here.
+    console.log('navigator', navigator)
+  })
+
+  /*
+  if (device === 'desktop') {
+    return Image()
+  } else {
+    return null
+  } */
+
+  return null
 }
 
 export default Avatar
