03855b6d9914e7abe661c6db8bac7fb8e0f0d435
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-03-21 21:39:41 +0200

    Blob animations: keyframes configurations.

---
+++ website/components/Animations/keyframe.js
@@ -0,0 +1,13 @@
+import { keyframes } from 'styled-components'
+
+export const left = keyframes`
+  0%{left:20%;}
+  50%{left:50%;}
+  100%{left:20%;}
+`
+
+export const right = keyframes`
+  0%{left:80%;}
+  50%{left:50%;}
+  100%{left:80%;}
+`
