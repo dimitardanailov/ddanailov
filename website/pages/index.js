067a7dec5c9bc42bdd41dc8ba0ee48dfbb181d2f
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-03-21 21:40:39 +0200

    Indexpage uses blob animation.

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,12 +1,13 @@
 import dynamic from 'next/dynamic'
 
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import MainLayout from '../layouts/MainLayout'
 
 import PersonalStory from '../components/PersonalStory'
 import OpenSourceProjects from '../components/OpenSourceProjects'
 import Skills from '../components/Skills'
+import Blob from '../components/Animations/Blob'
 // TODO: import ContactForm from '../components/FormComponents/ContactForm';
 
 const Sidebar = dynamic(() => import('../components/Sidebar'))
@@ -47,6 +48,7 @@ const Page = () => (
     <PageHolder>
       <Slogan>Dear Guest,</Slogan>
       <SocialIcons />
+      <Blob width="80%" size="40px" height="60px" rightBackground="#0ff" leftBlobBackground="#fff" />
       <PersonalStory />
       <OpenSourceProjects />
       <Skills />
