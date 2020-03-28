c04f8d36890b7db589b9ecf76eb9bec2198a14f2
Dimitar Danailov <dimityr.danailov@gmail.com>
2020-03-28 10:32:11 +0200

    MainLayout is more generic.

--- website/layouts/MainLayout.js
+++ website/layouts/MainLayout.js
@@ -1,7 +1,11 @@
+import dynamic from 'next/dynamic'
+
 import MainHeader from '../components/MainHeader'
 import MainFooter from '../components/MainFooter'
 
 import styled from 'styled-components'
+import PageTemplate from './components/PageTemplate'
+import PageHolder from './components/PageHolder'
 
 const Container = styled.section`
   position: relative;
@@ -9,10 +13,20 @@ const Container = styled.section`
   min-height: 100%;
 `
 
+const Sidebar = dynamic(() => import('../components/Sidebar'))
+
 const MainLayout = Page => () => (
   <Container>
     <MainHeader />
-    <Page />
+
+    <PageTemplate>
+      <Sidebar />
+
+      <PageHolder>
+        <Page />
+      </PageHolder>
+    </PageTemplate>
+
     <MainFooter />
   </Container>
 )
--- website/pages/index.js
+++ website/pages/index.js
@@ -10,27 +10,7 @@ import Skills from '../components/Skills'
 import Blob from '../components/Animations/Blob'
 // TODO: import ContactForm from '../components/FormComponents/ContactForm';
 
-const Sidebar = dynamic(() => import('../components/Sidebar'))
 const SocialIcons = dynamic(() => import('../components/SocialIcons'))
-
-const PageTemplate = styled.section`
-  position: relative;
-
-  display: flex;
-  min-height: 100%;
-  flex-direction: row-reverse;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
-`
-
-const PageHolder = styled.section`
-  position: relative;
-  margin: 1rem 1.2rem;
-
-  padding-bottom: 60px;
-`
 
 const Slogan = styled.div`
   position: relative;
@@ -42,24 +22,20 @@ const Slogan = styled.div`
 `
 
 const Page = () => (
-  <PageTemplate>
-    <Sidebar />
-
-    <PageHolder>
-      <Slogan>Dear Guest,</Slogan>
-      <SocialIcons />
-      <Blob
-        width="80%"
-        size="40px"
-        height="60px"
-        rightBackground="#0ff"
-        leftBlobBackground="#fff"
-      />
-      <PersonalStory />
-      <OpenSourceProjects />
-      <Skills />
-    </PageHolder>
-  </PageTemplate>
+  <>
+    <Slogan>Dear Guest,</Slogan>
+    <SocialIcons />
+    <Blob
+      width="80%"
+      size="40px"
+      height="60px"
+      rightBackground="#0ff"
+      leftBlobBackground="#fff"
+    />
+    <PersonalStory />
+    <OpenSourceProjects />
+    <Skills />
+  </>
 )
 
 export default MainLayout(Page)
