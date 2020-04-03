95b0dccb6e69bce0fa01ba861a1493b041ff5516
ddanailov <dimityr.danailov@gmail.com>
2020-04-03 22:25:45 +0300

    Blob animation is not used.

--- website/pages/index.js
+++ website/pages/index.js
@@ -5,7 +5,6 @@ import MainLayout from '@ddanailov/layouts/MainLayout'
 import PersonalStory from '@ddanailov/components/PersonalStory'
 import OpenSourceProjects from '@ddanailov/components/OpenSourceProjects'
 import Skills from '@ddanailov/components/Skills'
-import Blob from '@ddanailov/components/Animations/Blob'
 
 import Slogan from '@ddanailov/styled/Slogan'
 
@@ -15,13 +14,6 @@ const Page = () => (
   <>
     <Slogan>Dear Guest,</Slogan>
     <SocialIcons />
-    <Blob
-      width="80%"
-      size="40px"
-      height="60px"
-      rightBackground="#0ff"
-      leftBlobBackground="#fff"
-    />
     <PersonalStory />
     <OpenSourceProjects />
     <Skills />
