a32c915c521255c38a0f2ee9d55b9d2052a2482d
ddanailov <dimityr.danailov@gmail.com>
2020-04-06 09:52:31 +0300

    MainLayout doesn't use Sidebar.

--- website/layouts/MainLayout.js
+++ website/layouts/MainLayout.js
@@ -1,5 +1,3 @@
-import dynamic from 'next/dynamic'
-
 import MainHeader from '../components/MainHeader'
 import MainFooter from '../components/MainFooter'
 
@@ -13,15 +11,11 @@ const Container = styled.section`
   min-height: 100%;
 `
 
-const Sidebar = dynamic(() => import('../components/Sidebar'))
-
 const MainLayout = Page => () => (
   <Container>
     <MainHeader />
 
     <PageTemplate>
-      <Sidebar />
-
       <PageHolder>
         <Page />
       </PageHolder>
