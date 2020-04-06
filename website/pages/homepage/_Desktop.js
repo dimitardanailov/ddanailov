ad7e40f2fa4e83552fe4d88fe05bedfbe41f28d4
ddanailov <dimityr.danailov@gmail.com>
2020-04-06 08:55:16 +0300

    HomePageDesktop implementation.

---
+++ website/pages/homepage/_Desktop.js
@@ -0,0 +1,23 @@
+import dynamic from 'next/dynamic'
+
+import MainLayout from '@ddanailov/layouts/MainLayout'
+
+import PersonalStory from '@ddanailov/components/PersonalStory'
+import OpenSourceProjects from '@ddanailov/components/OpenSourceProjects'
+import Skills from '@ddanailov/components/Skills'
+
+import Slogan from '@ddanailov/styled/Slogan'
+
+const SocialIcons = dynamic(() => import('@ddanailov/components/SocialIcons'))
+
+const HomePageDesktop = () => (
+  <>
+    <Slogan>Dear Guest,</Slogan>
+    <SocialIcons />
+    <PersonalStory />
+    <OpenSourceProjects />
+    <Skills />
+  </>
+)
+
+export default MainLayout(HomePageDesktop)
