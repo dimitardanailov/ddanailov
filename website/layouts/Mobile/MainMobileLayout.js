b6d5a721053dfaa56a9112983d8b0a83f92f382e
ddanailov <dimityr.danailov@gmail.com>
2020-04-06 08:51:12 +0300

    Mobile Layout has a final version.

--- website/layouts/Mobile/MainMobileLayout.js
+++ website/layouts/Mobile/MainMobileLayout.js
@@ -1,12 +1,28 @@
+import styled from 'styled-components'
+
 import MainHeader from '@ddanailov/components/MainHeader'
 import MainFooter from '@ddanailov/components/MainFooter'
+import MobileSidebar from '@ddanailov/components/Sidebar/Mobille'
+
+const PageHolder = styled.section`
+  position: relative;
+
+  display: block;
+  padding: 0 0.75rem;
+  padding-bottom: 70px;
+`
 
 const MainMobileLayout = Page => () => (
   <>
     <MainHeader />
-    <Page />
 
-    <MainFooter />
+    <MobileSidebar />
+
+    <PageHolder>
+      <Page />
+
+      <MainFooter />
+    </PageHolder>
   </>
 )
 
