1521bb1701baaeae601e6f8fd2a22036f83d75bc
ddanailov <dimityr.danailov@gmail.com>
2020-04-06 08:56:42 +0300

    Simple styled component with Text info.

---
+++ website/components/Sidebar/Shared/Text.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const Text = styled.h4`
+  position: relative;
+
+  display: block;
+  text: align-center;
+  margin: 0;
+  padding: 0;
+
+  color: #f9f9f9;
+  text-align: center;
+
+  text-shadow: 0 0 4px #000;
+
+  font-size: 2.75rem;
+`
+
+export default Text
