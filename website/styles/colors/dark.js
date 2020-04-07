896b6c06a3245b372cf7c5825389fcc33ba11b84
ddanailov <dimityr.danailov@gmail.com>
2020-04-07 20:38:45 +0300

    Info about different styles.

---
+++ website/styles/colors/dark.js
@@ -0,0 +1,20 @@
+// grey
+const primary = {
+  main: '#212121',
+  light: '#484848',
+  dark: '#000000',
+}
+
+// blue
+const secondary = {
+  main: '#0d47a1',
+  light: '#5472d3',
+  dark: '#002171',
+}
+
+const themes = {
+  primary,
+  secondary,
+}
+
+export default themes
