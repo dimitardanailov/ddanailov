0e32b3fe99b3f5ef8589a8e2c76f1898f661a3d0
ddanailov <dimityr.danailov@gmail.com>
2020-04-07 21:20:31 +0300

    NavItem doesn't use list item.

--- website/components/Menu/NavItem.js
+++ website/components/Menu/NavItem.js
@@ -1,17 +1,16 @@
 import Link from 'next/link'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-const Item = styled.li`
-  padding: 0 0.5rem;
+
+const Href = styled.a`
+  color: inherit;
 `
 
 function NavItem({href, title, text}) {
   return (
-    <Item>
-      <Link href={href}>
-        <a title={title}>{text}</a>
-      </Link>
-    </Item>
+    <Link href={href}>
+      <Href title={title}>{text}</Href>
+    </Link>
   )
 }
 
