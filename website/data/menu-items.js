c5f8bd57800b121486a997b61bcffff4a74441f2
ddanailov <dimityr.danailov@gmail.com>
2020-04-07 20:38:28 +0300

    Info about navigation items.

---
+++ website/data/menu-items.js
@@ -0,0 +1,14 @@
+const items = [
+  {
+    text: 'Home',
+    href: '/',
+    title: '<ddanailov/>: Skils and Open source projects',
+  },
+  {
+    text: 'About',
+    href: '/about',
+    title: '<ddanailov/>: Testimonials and Recommendations',
+  },
+]
+
+export default items
