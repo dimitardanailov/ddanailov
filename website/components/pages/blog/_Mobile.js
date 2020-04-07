914c3be1993157ece04763a96478c696173fcb83
ddanailov <dimityr.danailov@gmail.com>
2020-04-07 22:10:29 +0300

    Simple mdx import

--- website/components/pages/blog/_Mobile.js
+++ website/components/pages/blog/_Mobile.js
@@ -1,9 +1,14 @@
 import MainMobileLayout from '@ddanailov/layouts/Mobile/MainMobileLayout'
 
+import Test from './Content.mdx'
+
 const BlogPageMobile = () => (
   <>
     <div>Hello world</div>
     <div>Hello world</div>
+    <MDXProvider>
+      <Test />
+    </MDXProvider>
   </>
 )
 
