5bec7bf50388e7cf797371795d20843786093eda
ddanailov <dimityr.danailov@gmail.com>
2020-04-10 09:35:05 +0300

    PWA setup.

--- website/components/MainHeader.js
+++ website/components/MainHeader.js
@@ -24,6 +24,25 @@ function MainHeader() {
         ></script>
 
         <base href="/" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="https://ddanailov.dev/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="https://ddanailov.dev/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="https://ddanailov.dev/favicon-16x16.png"
+        />
+        <link rel="manifest" href="https://ddanailov.dev/site.webmanifest" />
       </Head>
     </div>
   )
