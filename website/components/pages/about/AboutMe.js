e5647c21d9e37eec9531fe500b376c6e7f192d23
ddanailov <dimityr.danailov@gmail.com>
2020-04-21 23:48:55 +0300

    Component has details about my personal achievements.

---
+++ website/components/pages/about/AboutMe.js
@@ -0,0 +1,27 @@
+import Slogan from '@ddanailov/styled/Slogan'
+
+function AboutMe() {
+  return (
+    <>
+      <Slogan>Dimitar Danailov</Slogan>
+      <p>
+        Remote consultant / Full stack Web Developer with more than 10 years of
+        profesional experience
+      </p>
+
+      <ul>
+        <li>
+          Fine with both front-end (React.js, Native web components, D3.js,
+          Polymer and SPA) and back-end(Nodejs, Go)
+        </li>
+        <li>Amazon web services, Azure and Google Cloud are my best friends</li>
+        <li>Creating software is my hobby</li>
+        <li>Mentorship: I love to share my experience with the world</li>
+        <li>I love open source and Stackoverflow</li>
+        <li>I'm a guy with good self - education discipline</li>
+      </ul>
+    </>
+  )
+}
+
+export default AboutMe
