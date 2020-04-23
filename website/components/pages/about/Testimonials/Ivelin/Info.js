e7305dd88d2a9f8d474458415f77330cfc0ad80a
ddanailov <dimityr.danailov@gmail.com>
2020-04-23 21:54:19 +0300

    Info and Letter contains details about my supperpowers.

---
+++ website/components/pages/about/Testimonials/Ivelin/Info.js
@@ -0,0 +1,21 @@
+import PersonInfo from '@ddanailov/components/pages/about/Testimonials/Components/PersonInfo'
+
+import {ivelin} from 'data/testimonials'
+
+function IvelinInfo() {
+  return (
+    <>
+      <PersonInfo>
+        <TargetBlankLink
+          href={ivelin.linkedin.profile}
+          title={ivelin.linkedin.title}
+        >
+          {ivelin.name}
+        </TargetBlankLink>
+        , {ivelin.title}
+      </PersonInfo>
+    </>
+  )
+}
+
+export default IvelinInfo
