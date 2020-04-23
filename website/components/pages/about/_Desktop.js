800f1b4bb56bfd554e9251d31ed74eac40921ae9
ddanailov <dimityr.danailov@gmail.com>
2020-04-23 21:55:15 +0300

    Ivelin info and letter are replaced by components.

--- website/components/pages/about/_Desktop.js
+++ website/components/pages/about/_Desktop.js
@@ -11,6 +11,9 @@ import PersonInfo from '@ddanailov/components/Testimonials/PersonInfo'
 import AboutMe from '@ddanailov/components/pages/about/AboutMe'
 import Roles from '@ddanailov/components/pages/about/Roles'
 
+import IvelinLetter from '@ddanailov/components/pages/about/Testimonials/Ivelin/Letter'
+import IvelinInfo from '@ddanailov/components/pages/about/Testimonials/Ivelin/Info'
+
 const TestimonialBody = styled.div`
   position: relative;
 
@@ -93,46 +96,8 @@ const AboutPage = () => (
       />
 
       <RecommendationBody>
-        <p>
-          I am writing this recommendation for Dimitar Danailov with great
-          pleasure and respect. Dimitar has worked for me and with me for more
-          than five years in 158Ltd. He has started as programmer and passed all
-          the levels to CTO in the last year. In terms of technical skills, he
-          has an undeniable qualification, but for me and for the company he was
-          highly esteemed for his loyalty and devotion. He is a person with whom
-          I have worked with pleasure and I have had unreserved confidence in
-          him, since we have managed more than 100 projects in time and he has
-          proven his competence and his ability to work on time and with
-          quality. Rarely, we can meet people strongly devoted to their work,
-          however Dimitar is one of them, and the priceless thing for him is
-          that he transfers his energy and knowledge to the team, motivates them
-          and he is one good example.
-        </p>
-        <p>
-          Since the beginning he has demonstrated a strict compliance to the
-          working processes and rules of the company. By taking the position of
-          CTO in 158Ltd. Together with his help we have built the team from the
-          base of three programmers to the current 28. Over the past years, we
-          have participated in many risky projects and in his face I have always
-          had a confidential and pragmatic partner in making quick and right
-          decisions
-        </p>
-        <p>
-          In conclusion, let me say that without hesitation I recommend Dimitar
-          Danailov for the position lead programmer, related to software
-          development. I am sure that he is going to be an invaluable asset for
-          your organization. If you would like to discuss further my opinion and
-          assessment of Dimitar, please do not hesitate to contact me
-        </p>
-        <PersonInfo>
-          <TargetBlankLink
-            href={ivelin.linkedin.profile}
-            title={ivelin.linkedin.title}
-          >
-            {ivelin.name}
-          </TargetBlankLink>
-          , {ivelin.linkedin.title}
-        </PersonInfo>
+        <IvelinLetter />
+        <IvelinInfo />
       </RecommendationBody>
     </TestimonialBodyCenter>
 
