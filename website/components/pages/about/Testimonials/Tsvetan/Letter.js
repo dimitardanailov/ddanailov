bbc0a7ba73344a5bbc0df65b7348dd6cc2ba9fe5
ddanailov <dimityr.danailov@gmail.com>
2020-04-28 23:37:05 +0300

    TsvetanLetter is a component with main goal ro represent why we have a connection.

---
+++ website/components/pages/about/Testimonials/Tsvetan/Letter.js
@@ -0,0 +1,16 @@
+function TsvetanLetter() {
+  return (
+    <>
+      <p>
+        "Dimitar is a personable individual that gets on well with those around
+        him. He is focused and dedicated to producing his very best at all
+        times. He has shown that he is able to deal with uncertainty and
+        dislocation. He is very good software engineer knows a lot of
+        technologies. Also great teamleader. I recommend him highly – he will
+        not let you down!"
+      </p>
+    </>
+  )
+}
+
+export default TsvetanLetter
