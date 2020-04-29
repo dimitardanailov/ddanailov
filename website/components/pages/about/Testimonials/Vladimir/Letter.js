496cfd693248cb042ced23999a93bc8ba2d29b6f
ddanailov <dimityr.danailov@gmail.com>
2020-04-29 23:13:58 +0300

    Vladimir's Testimonial uses a component.

---
+++ website/components/pages/about/Testimonials/Vladimir/Letter.js
@@ -0,0 +1,15 @@
+function VladimirLetter() {
+  return (
+    <>
+      Dimitar is an excellent programmer and soul of the team. I am glad that I
+      be given the opportunity to recommend it because I have a very positive
+      impression of his personality. We spent over a year and a half working
+      together in 158ltd.com. and in that time he managed to pass me a lot of
+      knowledge in the IT field. Whenever I needed help or advice he has
+      responded and helped me. I'm sure Dimitar would be a great part of every
+      team.
+    </>
+  )
+}
+
+export default VladimirLetter
