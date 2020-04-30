f01fd3dc0a3f591b9d63568200a462c4937987dd
ddanailov <dimityr.danailov@gmail.com>
2020-04-30 21:56:34 +0300

    TsvetanInfo contains social links.

---
+++ website/components/pages/about/Testimonials/Tsvetan/Info.js
@@ -0,0 +1,17 @@
+import TargetBlankLink from '@ddanailov/components/TargetBlankLink'
+import PersonInfo from '@ddanailov/components/pages/about/Testimonials/Components/PersonInfo'
+
+import {tsvetan} from 'data/testimonials'
+
+function TsvetanInfo() {
+  return (
+    <PersonInfo>
+      <TargetBlankLink href={tsvetan.href} title={tsvetan.title}>
+        {tsvetan.name}
+      </TargetBlankLink>
+      , {tsvetan.title}
+    </PersonInfo>
+  )
+}
+
+export default TsvetanInfo
