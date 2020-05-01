c695837f16efd0d87c40b24f5d768f7160cab7cc
ddanailov <dimityr.danailov@gmail.com>
2020-05-01 22:22:25 +0300

    Desktop uses MoLetter component

--- website/components/pages/about/_Desktop.js
+++ website/components/pages/about/_Desktop.js
@@ -19,6 +19,9 @@ import HeidiInfo from '@ddanailov/components/pages/about/Testimonials/Heidi/Info
 import IvelinLetter from '@ddanailov/components/pages/about/Testimonials/Ivelin/Letter'
 import IvelinInfo from '@ddanailov/components/pages/about/Testimonials/Ivelin/Info'
 
+// Mo
+import MoLetter from '@ddanailov/components/pages/about/Testimonials/Mo/Letter'
+
 // Ashton
 import AshtonLetter from '@ddanailov/components/pages/about/Testimonials/Ashton/Letter'
 import AshtonInfo from '@ddanailov/components/pages/about/Testimonials/Ashton/Info'
@@ -120,43 +123,7 @@ const AboutPage = () => (
       />
 
       <RecommendationBody>
-        <p>
-          It is with great pleasure that I write this letter of recommendation
-          on behalf of (
-          <a
-            href="https://wollow-soft.com/en-US/"
-            title="Software Developer and CTO in Wollow soft"
-            rel="noopener"
-            target="_blank"
-          >
-            Wollow soft
-          </a>
-          ), a knowledgeable, dynamic, and flexible technology partner. What
-          impressed us most about Wollow’s approach to our project was their
-          vision and leadership, commitment to the customer, insistence on
-          setting realistic goals, with a succinct timelines, organization and
-          project management, education and coaching. Bar none, their cutting
-          edge technical expertise is of superior quality, delivered with a
-          business-like approach. But it was the “everything else” that
-          completed the package. Wollow’s success is due to its winning team.
-          The initial vision, leadership and project management of the CEO{' '}
-          <a
-            href="https://www.linkedin.com/in/ivelinivanov158/"
-            title="CEO - Wollow Soft"
-            rel="noopener"
-            target="_blank"
-          >
-            Ivelin Ivanov
-          </a>{' '}
-          paved the way for our project’s success, and the technical expertise
-          of the CTO Dimitar Danailov has provided a state-of-the-art platform
-          for the projects we’ve worked together on. As a company, we are fully
-          satisfied with the final product that Wollow has delivered. Based on
-          our experience, we would recommend to anyone interested the services
-          of Wollow since they are trusted and valuable long-term business
-          partner.
-        </p>
-
+        <MoLetter />
         <PersonInfo>
           <TargetBlankLink
             href="https://www.linkedin.com/in/mohamdouna/"
