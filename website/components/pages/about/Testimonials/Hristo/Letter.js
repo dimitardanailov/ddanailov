5730ce4c96a320df4c765e14f4a61155a9256f1b
ddanailov <dimityr.danailov@gmail.com>
2020-05-01 23:10:37 +0300

    Hristo's Testimonial details.

---
+++ website/components/pages/about/Testimonials/Hristo/Letter.js
@@ -0,0 +1,12 @@
+function HristoLetter() {
+  return (
+    <p>
+      "Dimitar is an outstanding mentor. His guidance helped me progress with
+      Ruby on Rails with ease and discover more about developing web-based
+      software. Even after my internship at 158ltd ended, he continued assisting
+      me with advice. Definitely a great person to learn from and to work with!"
+    </p>
+  )
+}
+
+export default HristoLetter
