5823808294e6762d045ce19fb7fe51b209cc258d
ddanailov <dimityr.danailov@gmail.com>
2020-05-04 23:30:05 +0300

    App will be used to hold all nextjs header info.

--- website/components/App.js
+++ website/components/App.js
@@ -1,1 +1,19 @@
-export default ({children}) => <div>{children}</div>
+import MainHeader from '@ddanailov/components/MainHeader'
+
+import PropTypes from 'prop-types'
+
+function App({canonicalTag, children}) {
+  return (
+    <>
+      <MainHeader canonicalTag={canonicalTag} />
+      {children}
+    </>
+  )
+}
+
+App.propTypes = {
+  canonicalTag: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+}
+
+export default App
