7c16c5f616efe253e902f21f4c7169e9f8595e2d
ddanailov <dimityr.danailov@gmail.com>
2020-05-05 22:38:02 +0300

    blog page uses Pages vs pages folder path.

--- website/pages/blog.js
+++ website/pages/blog.js
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic'
 import React, {useEffect} from 'react'
 
 import detectDevice from '@ddanailov/utils/devices/detectDevice'
-import Switch from '@ddanailov/components/pages/shared/devices/Switch'
+import Switch from '@ddanailov/components/Pages/shared/devices/Switch'
 
 const BlogPage = () => {
   const [device, setDevice] = React.useState('')
@@ -22,11 +22,11 @@ const BlogPage = () => {
       <Switch
         device={device}
         MobilePage={dynamic(
-          import('@ddanailov/components/pages/blog/_Mobile'),
+          import('@ddanailov/components/Pages/blog/_Mobile'),
           loading,
         )}
         DesktopPage={dynamic(
-          import('@ddanailov/components/pages/blog/_Desktop'),
+          import('@ddanailov/components/Pages/blog/_Desktop'),
           loading,
         )}
       />
