aa0786a2017d27a6d50b6a37eb4d73e58b7e7735
ddanailov <dimityr.danailov@gmail.com>
2020-05-06 23:09:45 +0300

    Avatars file replaces all sub Avatars.

---
+++ website/pages-nextjs/About/Components/Testimonials/Avatars.js
@@ -0,0 +1,71 @@
+import Avatar from '@about/Components/Avatar'
+
+import {
+  heidi,
+  henry,
+  ivelin,
+  mo,
+  tsvetan,
+  hristo,
+  vladimir,
+} from 'data/testimonials'
+
+function PersonAvatar({href, src, title}) {
+  return <Avatar href={href} src={src} title={title} />
+}
+
+export function HeidiAvatar() {
+  return PersonAvatar({
+    href: heidi.linkedin.profile,
+    src: heidi.linkedin.image,
+    title: heidi.title,
+  })
+}
+
+export function HenryAvatar() {
+  return PersonAvatar({
+    href: henry.linkedin.profile,
+    src: henry.linkedin.image,
+    title: henry.title,
+  })
+}
+
+export function IvelinAvatar() {
+  return PersonAvatar({
+    href: ivelin.linkedin.profile,
+    src: ivelin.linkedin.image,
+    title: ivelin.title,
+  })
+}
+
+export function MoAvatar() {
+  return PersonAvatar({
+    href: mo.linkedin.profile,
+    src: mo.linkedin.image,
+    title: mo.linkedin.title,
+  })
+}
+
+export function HristoAvatar() {
+  return PersonAvatar({
+    href: hristo.linkedin.profile,
+    src: hristo.linkedin.image,
+    title: hristo.title,
+  })
+}
+
+export function TsvetanAvatar() {
+  return PersonAvatar({
+    href: tsvetan.linkedin.profile,
+    src: tsvetan.linkedin.image,
+    title: tsvetan.title,
+  })
+}
+
+export function VladimirAvatar() {
+  return PersonAvatar({
+    href: vladimir.linkedin.profile,
+    src: vladimir.linkedin.image,
+    title: vladimir.title,
+  })
+}
