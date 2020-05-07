a6e87e3705e07478af4109cffd9d6d22afae27bd
ddanailov <dimityr.danailov@gmail.com>
2020-05-07 23:02:22 +0300

    StyledComponents can be reused.

---
+++ website/pages-nextjs/About/StyledComponents/index.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+const TestimonialBody = styled.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: row;
+
+  margin: 1rem 0;
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
+`
+
+const TestimonialBodyCenter = styled(TestimonialBody)`
+  align-items: center;
+`
+
+const RecommendationBody = styled.div`
+  position: relative;
+
+  text-align: justify;
+
+  margin-left: 2rem;
+
+  @media (max-width: 1024px) {
+    margin-left: 0rem;
+  }
+`
+
+export {TestimonialBody, TestimonialBodyCenter, RecommendationBody}
