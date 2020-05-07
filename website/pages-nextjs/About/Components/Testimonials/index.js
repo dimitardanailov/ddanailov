6faa09d66813140aec115a2be8de7d747faf13d5
ddanailov <dimityr.danailov@gmail.com>
2020-05-07 22:54:05 +0300

    Testimonials have a shorthand import.

---
+++ website/pages-nextjs/About/Components/Testimonials/index.js
@@ -0,0 +1,31 @@
+// Heidi
+export {default as HeidiLetter} from './Heidi/Letter'
+export {default as HeidiInfo} from './Heidi/Info'
+
+// Henry
+export {default as HenryLetter} from './Henry/Letter'
+export {default as HenryInfo} from './Henry/Info'
+
+// Ivelin
+export {default as IvelinLetter} from './Ivelin/Letter'
+export {default as IvelinInfo} from './Ivelin/Info'
+
+// Mo
+export {default as MoLetter} from './Mo/Letter'
+export {default as MoInfo} from './Mo/Info'
+
+// Ashton
+export {default as AshtonLetter} from './Ashton/Letter'
+export {default as AshtonInfo} from './Ashton/Info'
+
+// Hristo
+export {default as HristoLetter} from './Hristo/Letter'
+export {default as HristoInfo} from './Hristo/Info'
+
+// Tsvetan
+export {default as TsvetanLetter} from './Tsvetan/Letter'
+export {default as TsvetanInfo} from './Tsvetan/Info'
+
+// Vladimir
+export {default as VladimirLetter} from './Vladimir/Letter'
+export {default as VladimirInfo} from './Vladimir/Info'
