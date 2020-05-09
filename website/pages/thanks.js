5c5ef7cec41d70501481ebfd7efe9d475ce13943
ddanailov <dimityr.danailov@gmail.com>
2020-05-09 09:25:12 +0300

    Thanks page.

---
+++ website/pages/thanks.js
@@ -0,0 +1,29 @@
+import dynamic from 'next/dynamic'
+
+import React, {useEffect} from 'react'
+
+import App from '@ddanailov/components/App'
+import detectDevice from '@ddanailov/utils/devices/detectDevice'
+import Switch from '@ddanailov/components/Pages/shared/devices/Switch'
+import PageLoader from '@ddanailov/components/DynamicImports/PageLoader'
+
+const Homepage = () => {
+  const [device, setDevice] = React.useState('')
+
+  useEffect(() => {
+    const tempDevice = detectDevice()
+    setDevice(tempDevice)
+  }, [device])
+
+  return (
+    <App canonicalTag="/">
+      <Switch
+        device={device}
+        MobilePage={dynamic(import('@thanks/_Mobile'), PageLoader)}
+        DesktopPage={dynamic(import('@thanks/_Desktop'), PageLoader)}
+      />
+    </App>
+  )
+}
+
+export default Homepage
