6a58678097d399e5281b99e134ce81b89281d741
ddanailov <dimityr.danailov@gmail.com>
2020-05-10 10:47:48 +0300

    ThanksPage.Desktop uses MainDesktopLayout.

--- website/pages-nextjs/Thanks/_Desktop.js
+++ website/pages-nextjs/Thanks/_Desktop.js
@@ -1,19 +1,6 @@
-import dynamic from 'next/dynamic'
-
+import MainDesktopLayout from 'layouts/Desktop/MainDesktopLayout'
 import ThanksPageContent from '@thanks/Components/ThanksPageContent'
-import {PageTemplate, PageHolder} from '@ddanailov/styled/layouts/Desktop'
-import DesktopMenu from '@ddanailov/components/Menu/Desktop'
 
-const MainFooter = dynamic(() => import('@ddanailov/components/MainFooter'))
+const ThanksPage = () => <ThanksPageContent />
 
-const ThanksPage = () => (
-  <PageHolder>
-    <DesktopMenu />
-
-    <ThanksPageContent />
-
-    <MainFooter />
-  </PageHolder>
-)
-
-export default ThanksPage
+export default MainDesktopLayout(ThanksPage)
