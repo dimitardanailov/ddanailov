cb02eaa1f9b8f86a58ddbe346cf696da24762871
ddanailov <dimityr.danailov@gmail.com>
2020-05-11 23:37:55 +0300

    Profit page: Draft #1

--- website/pages-nextjs/Pages/01-profit/Components/ProfitPageContent.js
+++ website/pages-nextjs/Pages/01-profit/Components/ProfitPageContent.js
@@ -1,5 +1,58 @@
+import Slogan from '@ddanailov/styled/Slogan'
+
 function ProfitPageContent() {
-  return <>Hello world</>
+  return (
+    <>
+      <Slogan>From zero to 25000+ euro profit less than an year</Slogan>
+
+      <p>I want the article the summary / takeaway to be:</p>
+
+      <ul>
+        <li>You’re the boss</li>
+        <li>You’re the product</li>
+        <li>You need a good accountant</li>
+        <li>
+          You have the valuable contacts: Your mother, your neighbor and ...
+        </li>
+        <li>The biggest enemy is your fear of failure </li>
+        <li>The whole journey will change your life </li>
+        <li>Your business will make a stronger person</li>
+        <li>24/7 vs 40 working hours per week</li>
+        <li>The dreams are achievable</li>
+      </ul>
+
+      <Slogan>Why I'm sharing my journey ?</Slogan>
+
+      <p>
+        If more people follow their dream the earth will be a better place. The
+        working gap between 09am and 5pm is too old and unproductive. Each week
+        my goal is my schedule to have at least three or four powernaps. My
+        working schedule is most of the time are:
+      </p>
+
+      <ul>
+        <li>Jogging / Running - 15 minutes</li>
+        <li>Breakfast</li>
+        <li>Mediation - 10 minutes</li>
+        <li>I've a diary: 15 - 30 minutes</li>
+        <li>4 / 5 Working hours</li>
+        <li>Lunch</li>
+        <li>Powernap</li>
+        <li>2 Working hours</li>
+        <li>Workout</li>
+        <li>2 Working hours</li>
+        <li>Time with my family</li>
+      </ul>
+      <p>Question: Where is the dinner ?</p>
+      <p>Most of the time: I skip the dinner. I'm a big fan of 16/8 fasting.</p>
+
+      <Slogan>You’re the boss</Slogan>
+      <p>
+        In these eight years I met so many great leaders and friends. They
+        showed me : How you can build a successful business.
+      </p>
+    </>
+  )
 }
 
 export default ProfitPageContent
