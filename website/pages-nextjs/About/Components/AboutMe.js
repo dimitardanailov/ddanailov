073cc551c069097d32b692c0090f974f4f5b24c4
ddanailov <dimityr.danailov@gmail.com>
2020-05-20 23:56:38 +0300

    Quick link to my stackoverflow profile.

--- website/pages-nextjs/About/Components/AboutMe.js
+++ website/pages-nextjs/About/Components/AboutMe.js
@@ -1,5 +1,8 @@
 import dynamic from 'next/dynamic'
 
+import TargetBlank from '@ddanailov/components/TargetBlankLink'
+import Space from '@ddanailov/styled/Space'
+
 import Slogan from '@ddanailov/styled/Slogan'
 
 const SocialIcons = dynamic(() => import('@ddanailov/components/SocialIcons'))
@@ -13,7 +16,7 @@ function AboutMe() {
 
       <p>
         Remote consultant / Full stack Web Developer with more than 10 years of
-        profesional experience
+        profesional experience. My remote is experience began August 2018.
       </p>
 
       <ul>
@@ -24,7 +27,15 @@ function AboutMe() {
         <li>Amazon web services, Azure and Google Cloud are my best friends</li>
         <li>Creating software is my hobby</li>
         <li>Mentorship: I love to share my experience with the world</li>
-        <li>I love open source and Stackoverflow</li>
+        <li>
+          I love open source and <Space />
+          <TargetBlank
+            href="https://stackoverflow.com/users/609707/d-danailov"
+            title="My top tags are: Android, MySQL, Ruby on Rails, PHP and Javascript"
+          >
+            Stackoverflow
+          </TargetBlank>
+        </li>
         <li>I'm a guy with good self - education discipline</li>
       </ul>
     </>
