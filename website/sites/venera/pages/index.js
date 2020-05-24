44c48b5dec4bcdb7e5a9fab5a13cbe5672c1e25d
ddanailov <dimityr.danailov@gmail.com>
2020-05-24 23:02:22 +0300

    Application layer is able to use Keyword reusable component. The main issue is: The component can't be imported.

--- website/sites/venera/pages/index.js
+++ website/sites/venera/pages/index.js
@@ -1,7 +1,10 @@
+import Keyword from 'ddanailov-nextjs'
+
 export default function Index() {
   return (
     <div>
       <h1>Hello Venera 👋</h1>
+      <Keyword word="Hello" />
     </div>
   )
 }
