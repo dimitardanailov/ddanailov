cf2b34ccf0f6996b147354bf68a13384bdb98b19
ddanailov <dimityr.danailov@gmail.com>
2020-05-26 23:51:53 +0300

    The second article has a simple structure.

---
+++ website/pages-nextjs/Pages/02-material-ui-custom-list-item/_Desktop.js
@@ -0,0 +1,6 @@
+import MainDesktopLayout from 'layouts/Desktop/MainDesktopLayout'
+import Content from '@pages/02-material-ui-custom-list-item/Components/Content'
+
+const Page = () => <Content />
+
+export default MainDesktopLayout(Page)
---
+++ website/pages-nextjs/Pages/02-material-ui-custom-list-item/_Mobile.js
@@ -0,0 +1,12 @@
+import MainMobileLayout from 'layouts/Mobile/MainMobileLayout'
+import Content from '@pages/02-material-ui-custom-list-item/Components/Content'
+
+function Page() {
+  return (
+    <>
+      <Content />
+    </>
+  )
+}
+
+export default MainMobileLayout(Page)
