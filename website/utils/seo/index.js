5d924af13dd970c273d5ae7aea065d47d6a2b68c
ddanailov <dimityr.danailov@gmail.com>
2020-05-28 22:49:33 +0300

    Simple SEO class to hold all seo details.

---
+++ website/utils/seo/index.js
@@ -0,0 +1,27 @@
+class SEO {
+  get title() {
+    return this._title
+  }
+
+  set title(_title) {
+    this._title = _title
+  }
+
+  get canonicalTag() {
+    return this._canonicalTag
+  }
+
+  set canonicalTag(_canonicalTag) {
+    this._canonicalTag = _canonicalTag
+  }
+
+  get description() {
+    return this._description
+  }
+
+  set description(_description) {
+    this._description = _description
+  }
+}
+
+export default SEO
