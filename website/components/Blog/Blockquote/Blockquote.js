8309e33f5297520f5e3cd787c45165bb744e8ca6
ddanailov <dimityr.danailov@gmail.com>
2020-07-01 23:55:27 +0300

    Blockquote has a simple style.

--- website/components/Blog/Blockquote/Blockquote.js
+++ website/components/Blog/Blockquote/Blockquote.js
@@ -1,7 +1,17 @@
 import PropTypes from 'prop-types'
 
+import styled from 'styled-components'
+import {secondary} from 'styles/colors/light'
+
+const Wrapper = styled.blockquote`
+  position: relative;
+
+  display: block;
+  border: 1px solid ${secondary.main};
+`
+
 function BlockQuote({children}) {
-  return <>{children}</>
+  return <Wrapper>{children}</Wrapper>
 }
 
 BlockQuote.propTypes = {
