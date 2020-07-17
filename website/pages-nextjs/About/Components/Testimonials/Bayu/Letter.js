3141d4d728cd0f324c0e041bdd900758bec38e90
ddanailov <dimityr.danailov@gmail.com>
2020-07-17 22:50:53 +0300

    Bayu's recommendation letter was added.

---
+++ website/pages-nextjs/About/Components/Testimonials/Bayu/Letter.js
@@ -0,0 +1,21 @@
+function BauyLetter() {
+  return (
+    <>
+      <p>
+        WIth his experience, Mitco able to show awesome skill, knowledge and
+        critical thinking during working with him. He could play solo and also
+        collaboratively which making him easier to adapt various condition and
+        challenge.{' '}
+      </p>
+      <p>
+        He also loves to give some advice during discussion and in the same time
+        open for any input which making us grow together as a team. Will all of
+        this experience,
+      </p>
+      <p>
+        I believe he would be a great asset to any company or team. Good luck
+        Mitco!!!
+      </p>
+    </>
+  )
+}
