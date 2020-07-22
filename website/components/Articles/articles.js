62b2ec24275aea4ac4c3491acbca57ebe13a7c44
ddanailov <dimityr.danailov@gmail.com>
2020-07-22 22:33:28 +0300

    simple list with articles.

---
+++ website/components/Articles/articles.js
@@ -0,0 +1,28 @@
+const articles = [
+  {
+    title: 'How to protect my Ubuntu server ?',
+    href:
+      'https://medium.com/@d_danailov/how-to-protect-my-ubuntu-server-6a2ecc056722',
+  },
+  {
+    title: 'Git useful commands',
+    href: 'https://medium.com/@d_danailov/git-useful-commands-b3c4dcd0dd97',
+  },
+  {
+    title: 'Nodejs Microservice: Importing millions records on Amazon S3',
+    href:
+      'https://medium.com/@d_danailov/nodejs-microservice-importing-millions-records-on-amazon-s3-introduction-chapter-1-fafc208e40ad',
+  },
+  {
+    title: 'Productivity with VS Code (Tips and Tricks)',
+    href:
+      'https://medium.com/@d_danailov/productivity-with-vs-code-tips-and-tricks-51ae11e2e087',
+  },
+  {
+    title:
+      'Polymer vs Angular vs React: Using Web Components to Evolve the Experience',
+    href: 'https://mentormate.com/blog/polymer-vs-angular-future-web-apps/',
+  },
+]
+
+export default articles
