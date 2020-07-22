94030c0f674de40640c22d56220cd80e68d79b83
ddanailov <dimityr.danailov@gmail.com>
2020-07-22 22:34:30 +0300

    Component is responsible to present details about my technical knowledge.

--- website/components/Articles/ExternalArticles.js
+++ website/components/Articles/ExternalArticles.js
@@ -1,38 +1,11 @@
-const articles = [
-  {
-    title: 'How to protect my Ubuntu server ?',
-  },
-  {
-    title: 'Git useful commands',
-  },
-  {
-    title: 'Nodejs Microservice: Importing millions records on Amazon S3',
-  },
-  {
-    title: 'Productivity with VS Code (Tips and Tricks)',
-  },
-  {
-    title:
-      'Polymer vs Angular vs React: Using Web Components to Evolve the Experience',
-  },
-]
-
-function generateId(title) {
-  const id = title.replace(' ', '-').replace(':', '-')
-
-  return id
-}
-
-function Article({article}) {
-  return <h4>{article.title}</h4>
-}
+import List from './List'
 
 function ExternalArticles() {
   return (
     <>
-      {articles.map(article => {
-        return <Article key={generateId(article.title)} article={article} />
-      })}
+      <h3>Technical articles</h3>
+      <p>I want to share with you my technical knowledge:</p>
+      <List />
     </>
   )
 }
