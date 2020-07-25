125a68870dbc123eff8136f02c39d98cbd1165c1
ddanailov <dimityr.danailov@gmail.com>
2020-07-25 21:52:32 +0300

    Component is the main article title.

---
+++ website/components/Blog/Title/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const BlogTitle = styled.h1`
+  position: relative;
+
+  display: block;
+  font-size: 2rem;
+  font-weight: bold;
+
+  @media (max-width: 768px) {
+    text-align: center;
+  }
+`
+
+export default BlogTitle
