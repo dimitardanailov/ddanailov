8f1ef9625ccd882ee2d7b6dbad3a52e1fb47b507
ddanailov <dimityr.danailov@gmail.com>
2020-07-29 22:07:53 +0300

    Fanny's recommendation is a component.

---
+++ website/pages-nextjs/About/Components/Testimonials/Fanny/Info.js
@@ -0,0 +1,15 @@
+import TargetBlankLink from '@ddanailov/components/TargetBlankLink'
+import PersonInfo from '@about/Components/PersonInfo'
+
+function AshtonInfo() {
+  return (
+    <PersonInfo>
+      <TargetBlankLink href="https://www.linkedin.com/in/fanny-yap-2552156a/">
+        Fanny Yap
+      </TargetBlankLink>
+      , Product Manager GetCraft
+    </PersonInfo>
+  )
+}
+
+export default AshtonInfo
---
+++ website/pages-nextjs/About/Components/Testimonials/Fanny/Letter.js
@@ -0,0 +1,34 @@
+function FannyLetter() {
+  return (
+    <>
+      <p>
+        Being a non-technical lead PM, there are a number of instances where I
+        absolutely commend Dimitar taking the time in preparations visually and
+        verbally to share his perspectives, recommendations and solutions,
+        facilitating to helping me personally understand and navigate (tech)
+        better, with a goal of improving the process between design and
+        development. His positive agenda is to always continue to improve the
+        performance and usability of our platform, with our users experience in
+        mind.
+      </p>
+      <p>
+        Where communication can certainly sometimes prove to be challenging over
+        the initial teething period of us working with other coming from
+        different countries, there has been good personal and professional
+        rapport established over time, with a good number of laughs and in tow
+        over the last few months of collaborations. Projects I have worked on
+        with him being getting our new homepage up with the authentication and
+        navigations, and recently our managed services feature for sales
+        activations. No matter what the problem is, Dimitar always starts with
+        the right question, ie. 'Why?'.
+      </p>
+      <p>
+        Dimitar knowledge shares, keeping us updated on progress and adds
+        diversity to the way solutions are discovered. I have appreciated the
+        time we have worked together and sincerely thank Dimitar.
+      </p>
+    </>
+  )
+}
+
+export default FannyLetter
