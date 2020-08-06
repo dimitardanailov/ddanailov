0a9147154a1d45310e5a3ecb607026420075702e
ddanailov <dimityr.danailov@gmail.com>
2020-08-06 23:31:04 +0300

    The personal journey has a better structure.

--- website/components/PersonalStory.js
+++ website/components/PersonalStory.js
@@ -5,38 +5,32 @@ function PersonalStory() {
   return (
     <div>
       <div>
-        My personal journey began ten years ago. For this period I "visited" a
-        significant amount of kingdoms 👑. My roles were "warrior" (backend
-        developer), "engineer" (front - end developer), "magician" (team leader
-        and software architect), "the assassin" (freelancer /
+        My professional career started ten years ago. For this period I
+        "visited" a significant amount of kingdoms 👑. My roles were "warrior"
+        (backend developer), "engineer" (front - end developer), "magician"
+        (team leader and software architect), "the assassin" (freelancer /
         <Keyword word="remote contractor" />) and even the "main officer" (CTO).
-      </div>
-      <div>
-        When I worked like a warrior 💪 I wrote a significant amount of scripts
+        When I worked like a warrior 💪: I wrote a significant amount of scripts
         which automated some boring repetitive tasks for my stakeholders and the
-        end - users.
+        end - users. To be an engineer 🛠 is an awesome job because your main
+        focus is creating beautiful and helpful instruments. These instruments
+        reduced the main pain of each system: Adding, filtering data and state
+        management. The magician role 🧙‍♀️ was a tough and motivational task in my
+        career. My leadership skills cut the deadlines. The source code quality
+        was improved. The business won several important contracts. My skills as
+        an assassin 🔪 increased the company revenue. I've confident to work if
+        the deadlines are urgent.
       </div>
-      <div>
-        To be an engineer 🛠 is an awesome job because your main focus is
-        creating beautiful and helpful instruments which reduce the main pain of
-        each system: Adding or filtering data.
-      </div>
-      <div>
-        The magician role 🧙‍♀️ was a tough and motivational task. My leadership
-        skills cut the deadlines, improve code quality and the final result was:
-        strong and long partnership.
-      </div>
-      <div>
-        My skills as an assassin 🔪 increased the company revenue. I’m not
-        afraid to work until midnight. I've confident to work if the deadlines
-        are urgent.
-      </div>
-      <Link href="/about">
-        <a title="Dimitar Danailov: Portfolio details">
-          If you want to learn more about me: Please visit the second chapter of
-          my book.
-        </a>
-      </Link>
+      <p>
+        My winner mentality gave me an opportunity to work with remote teams in
+        Indonesia, Singapore, US, Australia, UK, Bulgaria.{' '}
+        <Link href="/about#about-testimonials-recommendations">
+          <a title="Dimitar Danailov: Portfolio details">
+            I received several five stars reviews from stakeholders, product
+            managers and devmates.
+          </a>
+        </Link>
+      </p>
     </div>
   )
 }
