fb789a738096de2a6fcc84f29b48d949a4fc44b6
ddanailov <dimityr.danailov@gmail.com>
2020-08-07 12:35:14 +0300

    Simple immer preferences reducer.

---
+++ website/reducers/preferences.js
@@ -0,0 +1,20 @@
+import produce from 'immer'
+
+export const preferencesRecipe = (draft, action) => {
+  switch (action.type) {
+    case 'SET_NIGHT_SHIFT_MODE':
+      const {nightShiftMode} = action
+      draft.nightShiftMode = nightShiftMode
+      break
+    default:
+      return getInitialState()
+  }
+}
+
+export const preferencesReducer = produce(preferencesRecipe)
+
+export function getInitialState() {
+  return {
+    nightShiftMode: 'default',
+  }
+}
