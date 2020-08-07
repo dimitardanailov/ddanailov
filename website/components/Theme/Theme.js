888db3561e3affbe0c1e0957f3303f4f6496abc5
ddanailov <dimityr.danailov@gmail.com>
2020-08-07 13:25:55 +0300

    The component is able to change the whole website configuration.

--- website/components/Theme/Theme.js
+++ website/components/Theme/Theme.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 
+import {ThemeProvider} from '@material-ui/core/styles'
+
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
@@ -8,19 +10,41 @@ import {
   getInitialState,
 } from '@ddanailov/reducers/preferences'
 
+import nightShiftModeThemeSetup from './configurations'
+import darkColors from './configurations/dark'
+import lightColors from './configurations/light'
+
+function isLightMode(mode) {
+  return mode === 'light' ? true : false
+}
+
 const Container = styled.section`
   position: relative;
 
   display: block;
   min-height: 100%;
   width: 100%;
+
+  background: ${props =>
+    isLightMode(props.nightShiftMode)
+      ? lightColors.primary.light
+      : darkColors.primary.light};
+  color: ${props =>
+    isLightMode(props.nightShiftMode)
+      ? darkColors.primary.light
+      : lightColors.primary.light};
 `
 
 function Theme({children}) {
   const [state, setState] = useState(() => getInitialState())
   const {nightShiftMode} = state
+  const themeSetup = nightShiftModeThemeSetup(nightShiftMode)
 
-  return <Container>{children}</Container>
+  return (
+    <ThemeProvider theme={themeSetup}>
+      <Container nightShiftMode={nightShiftMode}>{children}</Container>
+    </ThemeProvider>
+  )
 }
 
 Theme.propTypes = {
