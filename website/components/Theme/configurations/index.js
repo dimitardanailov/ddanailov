7dbb69aa5752a51f4395d94a0bd26fb645305676
ddanailov <dimityr.danailov@gmail.com>
2020-08-07 13:24:57 +0300

    Folder contains Material UI configuration theme configurations and color setup.

---
+++ website/components/Theme/configurations/index.js
@@ -0,0 +1,28 @@
+import {createMuiTheme} from '@material-ui/core/styles'
+
+import {
+  DEFAULT_STATE,
+  DARK_STATE,
+} from '@ddanailov/components/NightShiftMode/States'
+
+import lightColors from './light'
+import darkColors from './dark'
+
+function nightShiftModeThemeSetup(nightShiftMode) {
+  const palletState =
+    nightShiftMode === DEFAULT_STATE ? DEFAULT_STATE : DARK_STATE
+
+  return createMuiTheme({
+    palette: {
+      type: palletState,
+      primary: {
+        main: lightColors.primary.light,
+      },
+      secondary: {
+        main: darkColors.primary.dark,
+      },
+    },
+  })
+}
+
+export default nightShiftModeThemeSetup
