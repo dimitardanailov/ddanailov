7c6e77ac4678a887efe53cd335789a4512922eb3
ddanailov <dimityr.danailov@gmail.com>
2020-08-10 20:45:40 +0300

    Page title and description can be used as a global reference.

---
+++ website/data/seo.js
@@ -0,0 +1,19 @@
+const pages = {
+  home: {
+    title: 'Danailov Consulting: Javascript Consultant and Remote architect',
+    description:
+      'My skills are: React, Web Components, Cloud (Amazon, Google Cloud, Azure), Project leadership and mentorship',
+  },
+  about: {
+    title: 'Remote consultant with technology stack: React, Nodejs and Go',
+    description:
+      'Passion to work with React, Nodejs, Go, Amazon Web Services, Azure, Google Cloud',
+  },
+  contacts: {
+    title: 'Ready to start your next project with me?',
+    description:
+      'Finding a good software engineer can be a painful task. I can assure you that if you work with me once, you will always work with me for these kind of projects!',
+  },
+}
+
+export default pages
