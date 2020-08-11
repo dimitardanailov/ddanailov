d523c7459878cb6756170da1164e11f0d8f2c117
ddanailov <dimityr.danailov@gmail.com>
2020-08-11 11:31:23 +0300

    Abstract component with purpose: Form submit button.

---
+++ website/styled/form/BasicSubmit.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const BasicSubmit = styled.button`
+  position: relative;
+
+  display: block;
+  width: 100%;
+  padding: 1rem 0;
+  border-radius: 0.5rem;
+  border: none;
+
+  &:disabled {
+  }
+`
+
+export default BasicSubmit
