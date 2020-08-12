ba97aca797b4a5c544155c0771b4e1efaa5da894
ddanailov <dimityr.danailov@gmail.com>
2020-08-12 09:32:25 +0300

    ContactsPageContent is a basic style.

--- website/pages-nextjs/Contacts/Components/ContactsPageContent.js
+++ website/pages-nextjs/Contacts/Components/ContactsPageContent.js
@@ -1,15 +1,27 @@
+import styled from 'styled-components'
+
 import ContactForm from './ContactForm'
+
+const Wrapper = styled.section`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`
 
 function ContactsPageContent() {
   return (
-    <>
+    <Wrapper>
       <h2>Hire me as a software engineer</h2>
       <p>
         Ready to start your next project with me? Fill out the form below, and
         I'll be in touch!
       </p>
       <ContactForm />
-    </>
+    </Wrapper>
   )
 }
 
