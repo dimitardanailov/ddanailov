73da34303b543a993608f7af2121a2fc25f46ab6
ddanailov <dimityr.danailov@gmail.com>
2020-08-12 10:22:20 +0300

    Component has the style configurations.

--- website/styled/form/BasicField.js
+++ website/styled/form/BasicField.js
@@ -1,11 +1,24 @@
 import styled from 'styled-components'
 
+import {
+  opacity,
+  borderRadius,
+  boxShadow,
+} from '@ddanailov/styles/form/configurations'
+
 const BasicField = styled.input`
   position: relative;
 
   text-indent: 1rem;
   padding: 1rem 0;
-  border-radius: 0.5rem;
+  border-radius: ${borderRadius};
+
+  background-color: #fff;
+  color: #000;
+  border: 1px solid #e0e0e0;
+
+  box-shadow: ${boxShadow};
+  opacity: ${opacity};
 `
 
 export default BasicField
