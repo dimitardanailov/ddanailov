fafce34384849069d6b0797d77f3307a19000f4c
ddanailov <dimityr.danailov@gmail.com>
2020-08-26 23:02:27 +0300

    Each item has custom width and height dimensions.

--- website/demos/PinterestLayout/components/Item/index.js
+++ website/demos/PinterestLayout/components/Item/index.js
@@ -1,9 +1,28 @@
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+  position: relative;
+
+  display: flex;
+  border: 1px solid red;
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+`
 
 function Item({width, height}) {
-  console.log('width', width)
-  console.log('height', height)
-  return <>Item</>
+  return (
+    <>
+      <Wrapper width={width} height={height}>
+        Item
+      </Wrapper>
+    </>
+  )
+}
+
+Item.defaultProps = {
+  width: 100,
+  height: 100,
 }
 
 Item.propTypes = {
