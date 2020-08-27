225c60e306b983f1b484783c8218e16b5a4380dc
ddanailov <dimityr.danailov@gmail.com>
2020-08-27 22:41:38 +0300

    Layout component is able to override all configurations.

--- website/demos/PinterestLayout/index.js
+++ website/demos/PinterestLayout/index.js
@@ -1,11 +1,18 @@
 import Layout from './components/Layout'
 
+import config from './config'
 import records from './dummy/records.json'
 
 function PinterestLayout() {
   const items = records.items
 
-  return <Layout items={items} />
+  return (
+    <Layout
+      items={items}
+      maximumRowItems={config.maximumRowItems}
+      containerWidth={config.containerWidth}
+    />
+  )
 }
 
 export default PinterestLayout
