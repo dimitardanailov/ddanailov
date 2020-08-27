2cbfd11460c42c5ea76d06767bf838722fef20af
ddanailov <dimityr.danailov@gmail.com>
2020-08-27 22:41:03 +0300

    Configurations are in the seperated file.

---
+++ website/demos/PinterestLayout/config/index.js
@@ -0,0 +1,15 @@
+const config = {
+  maxItems: 50,
+  maximumRowItems: 5,
+  item: {
+    width: 200,
+    height: {
+      min: 50,
+      max: 399,
+    },
+  },
+}
+
+config['containerWidth'] = config.maximumRowItems * config.item.width
+
+module.exports = config
