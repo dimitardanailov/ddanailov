712387f3feb9cb5d147ea85509b97b4600b6a4e4
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-01-16 17:53:12 +0200

    The playground is able to get the closest color.

--- playgrounds/colors/index.js
+++ playgrounds/colors/index.js
@@ -1,1 +1,19 @@
-console.log("here ....");
+const nearestColor = require("nearest-color");
+const namedColors = require("color-name-list");
+
+function getClosesNamedColor(hex) {
+  // nearestColor need objects {name => hex} as input
+  const colors = namedColors.reduce(
+    (o, { name, hex }) => Object.assign(o, { [name]: hex }),
+    {}
+  );
+
+  const nearest = nearestColor.from(colors);
+
+  // get closest named color
+  const color = nearest(hex); // => Fairy Tale
+
+  return color;
+}
+
+console.log("colors", getClosesNamedColor("#f1c1d1"));
