657e3f6a0edfc33a3062a2a21630badd38244049
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-02-26 15:14:08 +0200

    The project convert raw color data to sorted array.

---
+++ website/mini-projects/colors.js
@@ -0,0 +1,29 @@
+const colors = require('./data.json')
+
+const quantization = calcQuantization(colors)
+console.log('quantization', quantization)
+
+function calcQuantization(colors) {
+  const map = new Map()
+
+  colors.forEach(color => {
+    if (map.has(color.doubleColorId)) {
+      const item = map.get(color.doubleColorId)
+      item.predominant = item.predominant + color.predominant
+    } else {
+      map.set(color.doubleColorId, color)
+    }
+  })
+
+  let arr = []
+
+  map.forEach(item => {
+    arr.push(item)
+  })
+
+  const output = arr.sort((a, b) => {
+    return b.predominant - a.predominant
+  })
+
+  return arr
+}
