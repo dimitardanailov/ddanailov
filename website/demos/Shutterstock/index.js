5c70401931804c004dfae53b3d2a768d11346c1f
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-04-02 23:24:16 +0300

    Application uses simple Shutterstock hello world.

--- website/demos/Shutterstock/index.js
+++ website/demos/Shutterstock/index.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+require('dotenv').config()
+
+const sstk = require("shutterstock-api");
+
+sstk.setAccessToken(process.env.SHUTTERSTOCK_API_TOKEN);
+
+const imagesApi = new sstk.ImagesApi();
+
+const queryParams = {
+  "query": "family",
+  "image_type": "photo",
+  "orientation": "vertical",
+  "people_number": 2
+};
+
+imagesApi.searchImages(queryParams)
+  .then(({ data }) => {
+    data.forEach(elements => {
+      console.log('elements', elements);
+    });
+    // console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
