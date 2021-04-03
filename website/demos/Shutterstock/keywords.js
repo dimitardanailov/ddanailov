0ba389bc17bceacbdd34cac3cac1d0707f783529
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-04-03 16:47:14 +0300

    The script is able to get keywords from image.

---
+++ website/demos/Shutterstock/keywords.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const sstk = require("shutterstock-api");
+const fs = require("fs");
+
+sstk.setAccessToken(process.env.SHUTTERSTOCK_API_TOKEN);
+
+const computerVisionApi = new sstk.ComputerVisionApi();
+
+const imageFile = fs.readFileSync("./lions.jpeg");
+const base64File = Buffer.from(imageFile).toString("base64");
+
+const body = new sstk.ImageCreateRequest(base64File);
+
+computerVisionApi.uploadImage(body)
+  .then((data) => {
+    console.log(data.upload_id);
+    return computerVisionApi.getKeywords(data.upload_id);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
