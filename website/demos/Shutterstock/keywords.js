b69a6733aae58df830709510088ab265d5b39566
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-04-03 21:47:54 +0300

    The workground has several examples.

--- website/demos/Shutterstock/keywords.js
+++ website/demos/Shutterstock/keywords.js
@@ -10,6 +10,9 @@ const computerVisionApi = new sstk.ComputerVisionApi();
 const images = [
   './images/lions.jpeg',
   './images/nws-st-bengal-tiger.jpeg',
+  './images/Kitchen-trends-2019-featured.jpeg',
+  './images/3VeiQCKVdLtqfez9KLG8iT.jpeg',
+  './images/bath-white.jpeg'
 ]
 
 const random = Math.floor(Math.random() * images.length)
