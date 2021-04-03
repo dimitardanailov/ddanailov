e16ac329e1b5ac4a1744ee76f383b27e68da47c4
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-04-03 22:50:56 +0300

    The source code is reusable.

--- website/demos/Shutterstock/categories.js
+++ website/demos/Shutterstock/categories.js
@@ -1,9 +1,4 @@
-require('dotenv').config()
-
-const sstk = require("shutterstock-api");
-sstk.setAccessToken(process.env.SHUTTERSTOCK_API_TOKEN);
-
-const imagesApi = new sstk.ImagesApi();
+const imagesApi = require('./_imagesApi')
 
 const queryParams = {
   "language": "en"
