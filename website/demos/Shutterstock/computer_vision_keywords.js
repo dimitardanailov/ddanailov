165a0bf58a5b2746a726ca2a2fb12adc42f587e9
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-04-04 20:17:11 +0300

    The script is able to generate random.

--- website/demos/Shutterstock/computer_vision_keywords.js
+++ website/demos/Shutterstock/computer_vision_keywords.js
@@ -2,23 +2,12 @@ require('dotenv').config()
 
 const sstk = require("shutterstock-api");
 const fs = require("fs");
+const image = require('./_computerVisionImages');
+console.log('image', image)
 
 sstk.setAccessToken(process.env.SHUTTERSTOCK_API_TOKEN);
 
 const computerVisionApi = new sstk.ComputerVisionApi();
-
-const images = [
-  './images/lions.jpeg',
-  './images/nws-st-bengal-tiger.jpeg',
-  './images/Kitchen-trends-2019-featured.jpeg',
-  './images/3VeiQCKVdLtqfez9KLG8iT.jpeg',
-  './images/bath-white.jpeg'
-]
-
-const random = Math.floor(Math.random() * images.length)
-const image = images[random]
-
-console.log('image', image)
 
 const imageFile = fs.readFileSync(image);
 const base64File = Buffer.from(imageFile).toString("base64");
