68c3774a2bbd5e267356d49fa3444ba3b030a52a
Dimitar Danailov <dimityr.danailov@gmail.com>
2021-12-02 13:13:11 +0200

    @format is a new option setup.

---
+++ website/demos/xero-test-ground/src/aws.ts
@@ -0,0 +1,60 @@
+/**
+ * @format
+ */
+import S3 from 'aws-sdk/clients/s3'
+import fs from 'fs'
+
+const bucketName = 'xero-development'
+const env = process.env.NODE_ENV || ''
+const fileName = env + '-token.json'
+const region = 'ap-southeast-1'
+const accessKeyId = process.env.XERO_AWS_ACCESS_KEY_ID || ''
+const secretAccessKey = process.env.XERO_AWS_SECRET_KEY || ''
+
+function init() {
+  const s3 = new S3({
+    region,
+    credentials: {
+      accessKeyId,
+      secretAccessKey,
+    },
+  })
+
+  return s3
+}
+
+function downloadToken(): Promise<string> {
+  const downloadParams = {
+    Key: fileName,
+    Bucket: bucketName,
+  }
+
+  const s3 = init()
+
+  const promise: Promise<string> = new Promise<string>(resolve => {
+    s3.getObject(downloadParams, function (_, data) {
+      const bucketData: string = data.Body?.toString('utf-8') || ''
+
+      resolve(bucketData)
+    })
+  })
+
+  return promise
+}
+
+function uploadFile(path: string) {
+  const fileContent = fs.readFileSync(path)
+  // console.log(fileStream)
+
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileContent,
+    Key: fileName,
+  }
+
+  const s3 = init()
+
+  return s3.upload(uploadParams).promise()
+}
+
+export {uploadFile, downloadToken}
