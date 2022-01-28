a0f030293796d951656a683b919b582ce94ada34
Dimitar Danailov <dimityr.danailov@gmail.com>
2022-01-28 13:45:22 +0200

    Function is able to convert public key from UTF8Array to string.

--- website/demos/algo-test-playground/src/services/createAddress.ts
+++ website/demos/algo-test-playground/src/services/createAddress.ts
@@ -1,14 +1,32 @@
 import algosdk from 'algosdk'
 
+declare var TextEncoder: any
+declare var TextDecoder: any
+
 const createAddress = () => {
   const account = algosdk.generateAccount()
-  console.log('account', account.addr)
-
-  // var secretKey = String.fromCharCode.apply(null, account.sk)
-  // console.log('Secret key', secretKey)
-
   const mn = algosdk.secretKeyToMnemonic(account.sk)
   console.log('Account Mnemonic:', mn)
+
+  console.log('account', account)
+
+  const {publicKey, checksum} = algosdk.decodeAddress(account.addr)
+  const address = algosdk.encodeAddress(publicKey)
+  console.log('account.addr === address', account.addr === address)
+  console.log('account.addr', account.addr)
+  console.log('address', address)
+  console.log('public key', publicKey)
+  console.log('checksum', checksum)
+
+  const buffer = Buffer.from(publicKey)
+  const pubKeyBase64: string = buffer.toString('base64')
+  const pubKeyArray: Uint8Array = Buffer.from(pubKeyBase64, 'base64')
+  const addr = algosdk.encodeAddress(pubKeyArray)
+  console.log('addr', addr)
+
+  return {
+    address: account.addr,
+  }
 }
 
 export default createAddress
