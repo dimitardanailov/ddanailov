bfea594e4d4cc91943f735da0a3500edcc806587
Dimitar Danailov <dimityr.danailov@gmail.com>
2022-02-08 11:46:48 +0200

    The application is able to convert public key to Uint8Array.

--- website/demos/algo-test-playground/src/services/createAddress.ts
+++ website/demos/algo-test-playground/src/services/createAddress.ts
@@ -14,8 +14,12 @@ const createAddress = () => {
   const pubKeyArray: Uint8Array = Buffer.from(pubKeyBase64, 'base64')
   const address = algosdk.encodeAddress(pubKeyArray)
   console.log('account.addr === address', account.addr === address)
-  console.log('account.addr', account.addr)
-  console.log('address', address)
+  console.log(
+    'account.addr',
+    account.addr,
+    algosdk.isValidAddress(account.addr),
+  )
+  console.log('address', address, algosdk.isValidAddress(address))
   console.log('public key', publicKey)
   console.log('checksum', checksum)
 
@@ -25,4 +29,14 @@ const createAddress = () => {
   }
 }
 
-export default createAddress
+const createAddressWithPublickKey = () => {
+  const publicKey =
+    '8ffc6992d34db8016f284fb2d84f5c5c38fb9c9ef523d92c2d54f76ce91f3857'
+
+  const pubKeyArray: Uint8Array = Uint8Array.from(Buffer.from(publicKey, 'hex'))
+  const address = algosdk.encodeAddress(pubKeyArray)
+
+  console.log('address', address, algosdk.isValidAddress(address))
+}
+
+export default createAddressWithPublickKey
