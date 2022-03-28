818a76280c2c096796e4e9c95bcf2314c0dd6a40
Dimitar Danailov <dimityr.danailov@gmail.com>
2022-03-28 09:01:29 +0300

    AtomService.requestAddressCreation is completed.

---
+++ website/demos/cosmos-playground/src/services/AtomService.ts
@@ -0,0 +1,48 @@
+import TransactionStatusCheck from '../interfaces/TransactionStatusCheck'
+import BigNumber from 'bignumber.js'
+import {DirectSecp256k1HdWallet} from '@cosmjs/proto-signing'
+import createCosmosAddressByEDCAKey from './address/createCosmosAddressByEDCAKey'
+
+enum KeyType {
+  ECDSA = 'ECDSA',
+  ED25519 = 'ED25519',
+}
+
+import createNewKeyPair from '../mockup/createNewKeyPair'
+import customPubkeyToAddress from '././address/createCosmosAddressByEDCAKey'
+
+export default class AtomService {
+  public keyType: KeyType
+  constructor() {
+    this.keyType = KeyType.ECDSA
+  }
+
+  async getBalance(address: string): Promise<string> {
+    return '0'
+  }
+
+  async getTransaction(txHash: string) {}
+
+  async getTransactions(address: string) {}
+
+  async getGasFee() {}
+
+  async getTransferValue(transactionHash: string) {}
+
+  async monitorTransaction(address: TransactionStatusCheck) {}
+
+  async getTransferFee(transactionHash: string): Promise<string> {
+    return '0'
+  }
+
+  async requestAddressCreation() {
+    const {publicKey, keyId} = await createNewKeyPair(this.keyType)
+    const address = await customPubkeyToAddress(publicKey)
+
+    return {
+      keyId,
+      publicKey,
+      address,
+    }
+  }
+}
