c9bf57b7bba7e6f236b41719af5c90fb10e5e65d
Dimitar Danailov <dimityr.danailov@gmail.com>
2022-04-07 14:48:28 +0300

    example how to compress node_modules in single file.

---
+++ website/demos/nodejs-tar/tar-npm-folder-single-file.js
@@ -0,0 +1,6 @@
+import tarNpmFolderSingleFile from './utils/tar-npm-folder-single-file.js'
+
+// import {tarNpmFolders} from 'tar-npm-modules'
+;(async () => {
+  await tarNpmFolderSingleFile('demo')
+})()
