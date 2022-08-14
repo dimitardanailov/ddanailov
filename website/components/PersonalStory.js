ec88c947ee0fc6f23d7f77e15679b8eb01d655bb
Dimitar Danailov <dimityr.danailov@gmail.com>
2022-08-14 05:43:39 +0300

    PersonalStory has a new version.

--- website/components/PersonalStory.js
+++ website/components/PersonalStory.js
@@ -5,26 +5,30 @@ function PersonalStory() {
   return (
     <div>
       <div>
-        My professional career started twelve years ago. For this period I
-        "visited" a significant amount of kingdoms 👑. My roles were "warrior"
-        (backend developer), "engineer" (front - end developer), "magician"
-        (team leader and software architect), "the assassin" (freelancer /
-        <Keyword word="remote contractor" />) and even the "main officer" (CTO).
-        When I worked like a warrior 💪: I wrote a significant amount of scripts
-        which automated some boring repetitive tasks for my stakeholders and the
-        end - users. To be an engineer 🛠 is an awesome job because your main
-        focus is creating beautiful and helpful instruments. These instruments
-        reduced the main pain of each system: Adding, filtering data and state
-        management. The magician role 🧙‍♀️ was a tough and motivational task in my
-        career. My leadership skills cut the deadlines. The source code quality
-        was improved. The business won several important contracts. My skills as
-        an assassin 🔪 increased the company revenue. I've confident to work if
-        the deadlines are urgent.
+        My professional journey began twelve years ago. During these years I
+        have “visited” a significant number of Kingdoms 👑. My roles were:
+        “Warrior” (backend developer), “Engineer” (frontend developer),
+        "Magician" (<Keyword word="team leader" />,{' '}
+        <Keyword word="software architect" />,{' '}
+        <Keyword word="blockchain staff engineer" />,{' '}
+        <Keyword word="Software Engineering Manager" />
+        ), "The assassin" (freelancer / <Keyword word="remote contractor" />)
+        and even the "Main officer" (CTO). While I was a Warrior 💪: I have
+        written a significant amount of scripts which automated some boring,
+        repetitive tasks for my stakeholders and the end - users. To be an
+        Engineer 🛠 is an awesome job because your main focus is creating
+        beautiful and helpful instruments. The components reduce the main pain
+        for each system: Filtering data, Adding data, Updating Data, Data
+        Visualization. To be a Magician 🧙‍♀️ was a tough and motivational task in
+        my career. My leadership skills cut the deadlines, improved code quality
+        and the final result was: strong and long partnership. My skills as an
+        assassin 🔪 increased the company revenue. I've confident to work on
+        several complicated projects.
       </div>
       <p>
         My winner mentality gave me an opportunity to work with remote teams in
-        Israel, Indonesia, Singapore, US, Australia, Russia, Ukraine, UK,
-        Bulgaria.{' '}
+        US, Israel, Indonesia, Singapore, Vietnam, Turkey, Australia, Russia,
+        Ukraine, UK, Bulgaria.{' '}
         <Link href="/about#about-testimonials-recommendations">
           <a title="Dimitar Danailov: Portfolio details">
             I received several five stars reviews from stakeholders, product
