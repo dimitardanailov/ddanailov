2025480778ab712d931d2a474b9de7b0ca843d30
Dimitar Danailov <dimityr.danailov@gmail.com>
2022-08-20 20:18:50 +0300

    AnnaMetelska Letter.

---
+++ website/pages-nextjs/About/Components/Testimonials/AnnaMetelska/Letter.js
@@ -0,0 +1,12 @@
+function AnnaMetelskaLetter() {
+  return (
+    <p>
+      It was a pleasure to work with Dimitar. He moderated Senior track of
+      Flutter Global Summit'21. He chose the best questions for speakers, that
+      covered topics. I can definitely recommend Dimitar like a reliable partner
+      for preparing online events.
+    </p>
+  )
+}
+
+export default AnnaMetelskaLetter
