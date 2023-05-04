69669ee6ea86e9e4cc23fed4cd8cd68d3180f414
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-05-04 08:34:37 +0300

    getBlockNumber returns info.

--- website/demos/ethersjs/src/helpers/getBlockNumber.ts
+++ website/demos/ethersjs/src/helpers/getBlockNumber.ts
@@ -1,6 +1,8 @@
 async function getBlockNumber(provider) {
   const blockNumber = await provider.getBlockNumber()
   console.log('blockNumber', blockNumber)
+
+  return blockNumber
 }
 
 export default getBlockNumber
